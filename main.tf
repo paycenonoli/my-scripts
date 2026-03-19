be151e2e9be05a560b5ba46ff00b0a757a547fe8
paycenonoli <119795130+paycenonoli@users.noreply.github.com>
2026-03-19 08:07:38 -0500

    Created main.tf

---
+++ main.tf
@@ -0,0 +1,31 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+}
+
+resource "aws_subnet" "subnet1" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.1.0/24"
+}
+
+resource "aws_security_group" "web_sg" {
+  vpc_id = aws_vpc.main.id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "web" {
+  ami           = "ami-123456"
+  instance_type = "t2.micro"
+  subnet_id     = aws_subnet.subnet1.id
+
+  vpc_security_group_ids = [aws_security_group.web_sg.id]
+}
